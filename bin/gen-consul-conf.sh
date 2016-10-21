f0b1fda138a9e1eed6bc6b064a37e56031be8e91
Greg Burek <greg.burek@gmail.com>
2016-10-21 13:39:11 -0700

    move to config gen

--- bin/gen-consul-conf.sh
+++ bin/gen-consul-conf.sh
@@ -3,23 +3,26 @@
 : "${HEROKU_DNS_APP_NAME:?Need to set HEROKU_DNS_APP_NAME non-empty}"
 : "${HEROKU_PRIVATE_IP:?Need to set HEROKU_PRIVATE_IP non-empty}"
 
-cat >> /app/vendor/consul/basic_config.json << EOFEOF
+$CONSUL_CONFIG_DIR = /app/vendor/consul/
+
+mkdir -p "$CONSUL_CONFIG_DIR"
+cat >> ${CONSUL_CONFIG_DIR}/basic_config.json << EOFEOF
 {
-  "bind_addr": $HEROKU_PRIVATE_IP,
+  "bind_addr": ${HEROKU_PRIVATE_IP},
   "bootstrap_expect": 3,
   "data_dir": '/tmp/consul/,
-  "datacenter": $HEROKU_DNS_APP_NAME,
+  "datacenter": ${HEROKU_DNS_APP_NAME}",
   "leave_on_terminate": true,
   "log_level": "INFO",
   "node_name": $(cat /proc/sys/kernel/random/uuid),
   "raft_multiplier": 1,
   "reconnect_timeout": "8h",
   "retry_join: [
-    "1.consul_server.$HEROKU_DNS_APP_NAME",
-    "2.consul_server.$HEROKU_DNS_APP_NAME",
-    "3.consul_server.$HEROKU_DNS_APP_NAME"
-    "4.consul_server.$HEROKU_DNS_APP_NAME",
-    "5.consul_server.$HEROKU_DNS_APP_NAME",
+    "1.consul_server.${HEROKU_DNS_APP_NAME}",
+    "2.consul_server.${HEROKU_DNS_APP_NAME}",
+    "3.consul_server.${HEROKU_DNS_APP_NAME}"
+    "4.consul_server.${HEROKU_DNS_APP_NAME}",
+    "5.consul_server.${HEROKU_DNS_APP_NAME}",
   ],
   "skip_leave_on_interrupt": false,
 }
